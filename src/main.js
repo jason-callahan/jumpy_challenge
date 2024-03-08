7bc7bca08a6d4f7d3c812162201a491b41f3602d
Jason Callahan <shamrockmedia@gmail.com>
2024-03-08 17:26:07 -0500

    exploring

---
+++ src/main.js
@@ -0,0 +1,81 @@
+import * as PIXI from "pixi.js";
+import { gsap } from "gsap";
+import { PixiPlugin } from "gsap/PixiPlugin";
+
+(async () => {
+    window.onload = async () => {
+        gsap.registerPlugin(PixiPlugin);
+        PixiPlugin.registerPIXI(PIXI);
+
+        const app = new PIXI.Application();
+        await app.init({
+            // width: window.innerWidth,
+            // height: window.innerHeight,
+            backgroundAlpha: 0,
+            resizeTo: window,
+        });
+
+        document.body.appendChild(app.canvas);
+        const texture = await PIXI.Assets.load("src/assets/sample.png");
+        const bunny = new PIXI.Sprite(texture);
+
+        bunny.x = app.renderer.width / 2;
+        bunny.y = app.renderer.height / 2;
+
+        bunny.anchor.set(0.5);
+
+        app.stage.addChild(bunny);
+
+        // gsap.to(bunny, {
+        //     pixi: {
+        //         scaleX: 2,
+        //         scaleY: 2,
+        //     },
+        //     duration: 2,
+        //     yoyo: true,
+        //     repeat: -1,
+        // });
+
+        bunny.on("pointerdown", (event) => {
+            console.log(event);
+            gsap.to(bunny, {
+                pixi: {
+                    scaleX: 2,
+                    scaleY: 2,
+                },
+                duration: 0.2,
+            });
+        });
+        bunny.on("pointerup", (event) => {
+            console.log(event);
+            gsap.to(bunny, {
+                pixi: {
+                    scaleX: 1,
+                    scaleY: 1,
+                },
+                duration: 0.2,
+            });
+        });
+        bunny.on("pointerleave", (event) => {
+            console.log(event);
+            gsap.to(bunny, {
+                pixi: {
+                    scaleX: 1,
+                    scaleY: 1,
+                },
+                duration: 0.2,
+            });
+        });
+        bunny.eventMode = "dynamic";
+
+        app.ticker.add(() => {
+            bunny.rotation += 0.01;
+        });
+
+        window.addEventListener("resize", () => {
+            // app.renderer.resize(window.innerWidth, window.innerHeight);
+            bunny.x = app.renderer.width / 2;
+            bunny.y = app.renderer.height / 2;
+        });
+    };
+})();
